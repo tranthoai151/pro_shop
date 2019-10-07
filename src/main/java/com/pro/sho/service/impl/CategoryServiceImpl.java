f3f79faab0f4d614346e055ef0796f7dcad77163
tranthoai151 <tranthoai142@gmail.com>
2019-10-07 16:12:03 +0700

    update autowrite

--- src/main/java/com/pro/sho/service/impl/CategoryServiceImpl.java
+++ src/main/java/com/pro/sho/service/impl/CategoryServiceImpl.java
@@ -6,6 +6,7 @@ import com.pro.sho.service.dto.CategoryDTO;
 import com.pro.sho.service.mapper.CategoryMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.data.domain.Page;
@@ -15,19 +16,18 @@ public class CategoryServiceImpl implements CategoryService {
 
     private final Logger log = LoggerFactory.getLogger(CategoryServiceImpl.class);
 
-    private final CategoryRepository categoryRepository;
+    @Autowired
+    private CategoryRepository categoryRepository;
 
-    private final CategoryMapper categoryMapper;
+    @Autowired
+    private CategoryMapper categoryMapper;
 
-
-    public CategoryServiceImpl(CategoryRepository categoryRepository, CategoryMapper categoryMapper) {
-        this.categoryRepository = categoryRepository;
-        this.categoryMapper = categoryMapper;
-    }
+    public CategoryServiceImpl(){}
 
     @Override
     public Page<CategoryDTO> searchCategoryByName(String name, Pageable pageable) {
         log.debug("Request to search Category : {}", name);
+        name = (name == null) ? "" : name;
         return categoryRepository.findAllByName(name, pageable).map(categoryMapper::toDto);
     }
 }
